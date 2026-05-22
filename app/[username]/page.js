b04e43c3a055e76d55f99a6579ff8e2741b3dab1
Akash Gautam <panditaakashji111@gmail.com>
2026-05-22 12:15:17 +0530

    feat: Implement creator fetching and add global 404 page

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,7 +1,18 @@
 import PaymentPage from '@/components/PaymentPage'
+import { fetchCreator } from '@/actions/userAction'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
-const page = ({params}) => {
+const page = async ({params}) => {
+  try {
+    const creator = await fetchCreator(params.username)
+    if (!creator) {
+      notFound()
+    }
+  } catch (error) {
+    notFound()
+  }
+
   return (
     <>
     <PaymentPage username={params.username} />
